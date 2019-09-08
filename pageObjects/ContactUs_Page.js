7cef4fbf9f9150da262a4f8693fa78ccd08cc578
Harshgiri Gonsai <30454569+giri-gonzai@users.noreply.github.com>
2019-09-08 17:11:43 +1200

    Final POM Abstraction with bug fixes

--- pageObjects/ContactUs_Page.js
+++ pageObjects/ContactUs_Page.js
@@ -14,40 +14,53 @@ class ContactUs_Page {                                      //Creating POM Class
     get submitButton() {
         return $("[type='submit']");
     }
-    get successfulSubmission() {
-        return $("#contact_reply h1");
-    }
-    get unsuccessfulSubmission() {
-        return $("body");
-    }
-}
 
-    function setFirstName(firstName){                           //Advanced POM Framwork Abstraction
+    setFirstName(firstName){                           //Advanced POM Framwork Abstraction
         return this.firstName.setValue(firstName);
     } 
-    function setLastName(lastName){
+    setLastName(lastName){
         return this.lastName.setValue(lastName);
     }
-    function setEmailAddress(emailAddress){
+    setEmailAddress(emailAddress){
         return this.emailAddress.setValue(emailAddress);
     }
-    function setMessage(comments){
+    setMessage(comments){
         return this.comments.setValue(comments);
     }
-    function clickSubmitButton(){
+    clickSubmitButton(){
         return this.submitButton.click();
     }
-    function confirmSuccessfullSubmission() {
+
+    submitAllInformationContactUsForm(firstName, lastName, emailAddress, comments) {
+        if(firstName) {
+            this.firstName.setValue(firstName);
+        }
+        if(lastName) {
+            this.lastName.setValue(lastName);
+        }
+        if(emailAddress) {
+            this.emailAddress.setValue(emailAddress);
+        }
+        if(comments) {
+            this.comments.setValue(comments);
+        }
+        this.submitButton.click();
+        this.confirmSuccessfullSubmission();
+    }
+
+    confirmSuccessfullSubmission() {
+        var successfulSubmission = "#contact_reply";
         var validateSubmissionHeader = browser.waitUntil(function() {
-            return this.successfulSubmission.getText() == 'Thank You for your Message!'
+            return browser.getText(successfulSubmission) == 'Thank You for your Message!'
     }, 3000);
         expect(validateSubmissionHeader, 'Successful Submission Message does not Exist!').to.be.true;
     }
-    function confirmUnsuccessfullSubmission() {
+    confirmUnsuccessfullSubmission() {
+        var unsuccessfulSubmission = "body";
         var validateSubmissionHeader = browser.waitUntil(function() {
-            return this.unsuccessfulSubmission.getText() == 'Error: all fields are required'
+            return browser.getText(unsuccessfulSubmission) == 'Error: all fields are required'
     }, 3000);
-        expect(this.unsuccessfulSubmission.getText()).to.include('Error: all fields are required');
+        expect(browser.getText(unsuccessfulSubmission)).to.include('Error: all fields are required');
     }
-
+}
 module.exports = new ContactUs_Page();
--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -6,39 +6,30 @@ beforeEach('Accessing Contact Us URL', function() {
 });
 
 describe('WebdriverUni: Test Contact Us Page', function() {     //Following POM Phase 1
-    it('Positive Test: Should be able to submit a successful submission via contact us form', function(done) {
-        
-        setFirstName('GG');
-        setLastName('Gogo');
-        setEmailAddress('gg@mail.com');
-        setMessage('NY');
-        clickSubmitButton();
-        confirmSuccessfullSubmission();         
+    it('Positive Test: Should be able to submit a successful submission via contact us form', function(done) {  
+        ContactUs_Page.submitAllInformationContactUsForm('GG', 'Gogo', 'gg@mail.com', 'NY');
         });
 
     it('Negative Test: Should not be able to submit a successful submission via contact us form as all field are required', function(done) {
-    
-        setFirstName('GG');
-        setLastName('Gogo');
-        setEmailAddress('gg@mail.com');
-        clickSubmitButton();
-        confirmUnsuccessfullSubmission();
+        ContactUs_Page.setFirstName('GG');
+        ContactUs_Page.setLastName('Gogo');
+        ContactUs_Page.setEmailAddress('gg@mail.com');
+        ContactUs_Page.clickSubmitButton();
+        ContactUs_Page.confirmUnsuccessfullSubmission();
         });
 
     it('Negative Test: Should not be able to submit a successful submission via contact us form as all field are required', function(done) {
-
-        setFirstName('GG');
-        setEmailAddress('gg@mail.com');
-        clickSubmitButton();
-        confirmUnsuccessfullSubmission();
+        ContactUs_Page.setFirstName('GG');
+        ContactUs_Page.setEmailAddress('gg@mail.com');
+        ContactUs_Page.clickSubmitButton();
+        ContactUs_Page.confirmUnsuccessfullSubmission();
         });
 
     it('Negative Test: Should not be able to submit a successful submission via contact us form as all field are required', function(done) {
-       
-        setLastName('Gogo');
-        setEmailAddress('gg@mail.com');
-        setMessage('NY');
-        clickSubmitButton();
-        confirmUnsuccessfullSubmission();
+        ContactUs_Page.setLastName('Gogo');
+        ContactUs_Page.setEmailAddress('gg@mail.com');
+        ContactUs_Page.setMessage('NY');
+        ContactUs_Page.clickSubmitButton();
+        ContactUs_Page.confirmUnsuccessfullSubmission();
         }); 
 });
