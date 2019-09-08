ce6c5405fd2321ed6e8f555413c4ffae900dfe37
Harshgiri Gonsai <30454569+giri-gonzai@users.noreply.github.com>
2019-09-08 15:57:51 +1200

    Advanced POM Abstraction: Phase - 3

--- pageObjects/ContactUs_Page.js
+++ pageObjects/ContactUs_Page.js
@@ -22,4 +22,32 @@ class ContactUs_Page {                                      //Creating POM Class
     }
 }
 
+    function setFirstName(firstName){                           //Advanced POM Framwork Abstraction
+        return this.firstName.setValue(firstName);
+    } 
+    function setLastName(lastName){
+        return this.lastName.setValue(lastName);
+    }
+    function setEmailAddress(emailAddress){
+        return this.emailAddress.setValue(emailAddress);
+    }
+    function setMessage(comments){
+        return this.comments.setValue(comments);
+    }
+    function clickSubmitButton(){
+        return this.submitButton.click();
+    }
+    function confirmSuccessfullSubmission() {
+        var validateSubmissionHeader = browser.waitUntil(function() {
+            return this.successfulSubmission.getText() == 'Thank You for your Message!'
+    }, 3000);
+        expect(validateSubmissionHeader, 'Successful Submission Message does not Exist!').to.be.true;
+    }
+    function confirmUnsuccessfullSubmission() {
+        var validateSubmissionHeader = browser.waitUntil(function() {
+            return this.unsuccessfulSubmission.getText() == 'Error: all fields are required'
+    }, 3000);
+        expect(this.unsuccessfulSubmission.getText()).to.include('Error: all fields are required');
+    }
+
 module.exports = new ContactUs_Page();
--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -6,35 +6,6 @@ beforeEach('Accessing Contact Us URL', function() {
 });
 
 describe('WebdriverUni: Test Contact Us Page', function() {     //Following POM Phase 1
-
-    function setFirstName(firstName){ 
-        return ContactUs_Page.firstName.setValue(firstName);
-    } 
-    function setLastName(lastName){
-        return ContactUs_Page.lastName.setValue(lastName);
-    }
-    function setEmailAddress(emailAddress){
-        return ContactUs_Page.emailAddress.setValue(emailAddress);
-    }
-    function setMessage(comments){
-        return ContactUs_Page.comments.setValue(comments);
-    }
-    function clickSubmitButton(){
-        return ContactUs_Page.submitButton.click();
-    }
-    function confirmSuccessfullSubmission() {
-        var validateSubmissionHeader = browser.waitUntil(function() {
-            return ContactUs_Page.successfulSubmission.getText() == 'Thank You for your Message!'
-        }, 3000);
-        expect(validateSubmissionHeader, 'Successful Submission Message does not Exist!').to.be.true;
-    }
-    function confirmUnsuccessfullSubmission() {
-        var validateSubmissionHeader = browser.waitUntil(function() {
-            return ContactUs_Page.unsuccessfulSubmission.getText() == 'Error: all fields are required'
-        }, 3000);
-        expect(ContactUs_Page.unsuccessfulSubmission.getText()).to.include('Error: all fields are required');
-    }
-   
     it('Positive Test: Should be able to submit a successful submission via contact us form', function(done) {
         
         setFirstName('GG');
