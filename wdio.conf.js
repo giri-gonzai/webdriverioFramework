965773e3073174c73374e84a73c2b394172abbff
Harshgiri Gonsai <30454569+giri-gonzai@users.noreply.github.com>
2019-09-08 18:03:15 +1200

    Adding Reports: JSON

--- wdio.conf.js
+++ wdio.conf.js
@@ -133,12 +133,15 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: http://webdriver.io/guide/reporters/dot.html
-    reporters: ['dot', 'junit'],
+    reporters: ['dot', 'junit', 'json'],
 
         reporterOptions: {
             junit: {
                 outputDir: './reports/junit-results/'
-            }
+            },
+            json: {
+                outputDir: './reports/json-results/'
+            },
         },
     //
     // Options to be passed to Mocha.
